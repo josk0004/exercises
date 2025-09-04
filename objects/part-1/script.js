2f7ecd510bd2f7bcb67fdfda0cd6602b5bebbc30
JoDe <jodejpg@gmail.com>
2025-09-04 12:37:35 +0200

    add object oevelse & language_switcher

---
+++ objects/part-1/script.js
@@ -0,0 +1,18 @@
+const student1 = {
+    firstName: "Harry",
+    lastName: "Potter",
+};
+
+const student2 = {
+    firstName: "Harry",
+    lastName: "Potter",
+};
+
+student1.firstName = student2.firstName;
+student1.lastName = student2.lastName;
+
+if (student1 === student2) {
+    console.log("The two students are the same.");
+} else {
+    console.log("The two students are different.");
+}
